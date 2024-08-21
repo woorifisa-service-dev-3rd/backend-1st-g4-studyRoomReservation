84453b650bde2c15363378da25d1f8d51dd196f3
Kong-E <kng001016@dgu.ac.kr>
2024-08-21 11:08:39 +0900

    feat: User 모델 생성
    issue #2

--- weird_quiz/src/main/java/model/User.java
+++ weird_quiz/src/main/java/model/User.java
@@ -1,2 +1,15 @@
-package model;public class user {
+package model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class User {
+    private int id;
+    private String name;
+    private int gameAttemptCount;
+    private int gameSuccessCount;
+    private int quizSolvedCount;
+    private int quizCorrectCount;
 }
